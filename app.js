e789d1f1ca20afdf6fc54a26bd9e2b4bdd757a90
Algram <aliasgram@gmail.com>
2016-03-30 14:23:35 +0200

    Refactored to have channel and not video

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var database = require('./config/database');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var videos = require('./routes/videos');
+var channel = require('./routes/channel');
 
 var app = express();
 
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/videos', videos);
+app.use('/channel', channel);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/channel.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+
+//Load all the things necessary for the api
+var google = require('googleapis');
+var youtube = google.youtube('v3');
+var api = require('../config/youtube_api');
+
+/* GET video listing for a channel. */
+router.get('/', function(req, res, next) {
+  if (req.query.name !== undefined) {
+    getChannel(req.query.name, function(channel) {
+      res.send(channel);
+    });
+  } else {
+    res.send();
+  }
+});
+
+
+//TODO add error handling (channel has no videos)
+function getChannel(channelName, cb) {
+  var cumVideoViews = 0;
+  var numOfVideos = 50;
+
+  var channel = {
+    name: channelName,
+    thumbnail: '',
+    avgVideoViews: 0,
+    videos: []
+  };
+
+  //Get channel with channelName
+  youtube.channels.list({ auth: api.key, part: 'contentDetails, snippet', forUsername: channelName}, function(err, data) {
+    var uploadPlaylistId = data.items[0].contentDetails.relatedPlaylists.uploads;
+
+    //Set channel thumbnail
+    channel.thumbnail =  data.items[0].snippet.thumbnails.medium.url;
+
+    //Get upload playlist
+    youtube.playlistItems.list({ auth: api.key, part: 'contentDetails', playlistId: uploadPlaylistId, maxResults: numOfVideos}, function(err, data) {
+      var playlistItems = data.items;
+      var videoCount = 0;
+
+      for (var key in playlistItems) {
+        var videoId = playlistItems[key].contentDetails.videoId;
+
+        //Get videos from uploads
+        youtube.videos.list({ auth: api.key, part: 'statistics, snippet', id: videoId}, function(err, data) {
+          var title = data.items[0].snippet.title;
+          var description = data.items[0].snippet.description;
+          var thumbnail = data.items[0].snippet.thumbnails.medium.url;
+          var viewCount = data.items[0].statistics.viewCount;
+          cumVideoViews += parseInt(viewCount);
+
+          channel.videos.push({
+            title: title,
+            description: description,
+            thumbnail: thumbnail,
+            viewCount: viewCount
+          });
+
+
+          videoCount++;
+
+          //Check if all requests are finished and call cb
+          if (videoCount === 50) {
+            //Add average number of views a video on this channel has
+            channel.avgVideoViews = Math.floor(cumVideoViews/numOfVideos);
+
+            //Remove videos that are below average
+            channel.videos = cullVideos(channel.videos, channel.avgVideoViews);
+
+            cb(channel);
+          }
+        });
+      }
+    });
+  });
+}
+
+function cullVideos(arr, avgVideoViews) {
+  var culledVideos = [];
+  for (var key in arr) {
+    var video = arr[key];
+
+    if (video.viewCount > avgVideoViews) {
+      culledVideos.push(video);
+    }
+  }
+
+  return culledVideos;
+}
+
+module.exports = router;
