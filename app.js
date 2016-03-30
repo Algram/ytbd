b7d1f4b91c228cb123388d3469afab5c75bc522d
Algram <aliasgram@gmail.com>
2016-03-30 13:18:06 +0200

    Added route for videos

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var database = require('./config/database');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var videos = require('./routes/videos');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/videos', videos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
